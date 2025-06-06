48438ffd9639ba576866afd35261a181c6f8caa1
Akshay B <25bakshay2004@gmail.com>
2025-06-06 23:33:41 +0530

    testing workflow functionality of actions

--- services/analytics-service/main.py
+++ services/analytics-service/main.py
@@ -4,6 +4,7 @@ from app.routes.analytics import router as analytics_router
 app = FastAPI()
 
 app.include_router(analytics_router, prefix="/analytics")
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=4003, reload=True)
